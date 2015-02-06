cdbf2cfe7ebd62d06175ea336c2026e08939e01d
EmanuelaMollova <emanuela.mollova@gmail.com>
2015-02-06 03:01:54 +0200

    Add title to layout

--- src/templates/layout.php
+++ src/templates/layout.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>[@title]</title>
+    <title>Students Achievements System</title>
     <meta charset='utf-8'>
 
     <link rel="stylesheet" type="text/css" href="/sas/src/public/css/style.css" />
