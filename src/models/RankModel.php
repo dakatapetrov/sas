158cd3bb15a29b017ad2d9b92c508fbf90ede2ae
EmanuelaMollova <emanuela.mollova@gmail.com>
2015-02-06 06:17:29 +0200

    Add some db methods for ranking

--- src/models/RankModel.php
+++ src/models/RankModel.php
@@ -22,12 +22,73 @@ class RankModel extends Model
     public function getUniqueSpecialities()
     {
         $query = $this->dbConnection->query(
-            'SELECT DISTINCT(name) FROM specialities'
+            'SELECT * FROM specialities'
         );
 
         $data = array();
         while ($line = $query->fetch_assoc()) {
-            $data[] = $line['name'];
+            $data[$line['id']] = $line['name'];
+        }
+
+        return $data;
+    }
+
+    public function getIntervals($period)
+    {
+        $fromDate = new \DateTime('now', new DateTimeZone('Europe/Sofia'));
+        $fromDate->modify('-1 ' . $period);
+        $fromDate->setTime(0, 0, 0);
+
+        $toDate = new \DateTime('now', new DateTimeZone('Europe/Sofia'));
+        $toDate->setTime(0, 0, 0);
+
+        return array('from' => $fromDate->format('Y-m-d H:i:s'), 'to' => $toDate->format('Y-m-d H:i:s'));
+    }
+
+    public function getBestStudents($class, $speciality, $interval)
+    {
+        $whereCondition = false;
+
+        $sql = 'SELECT s.user_id, s.fn, u.first_name, u.last_name, SUM(points) as points
+                FROM students_achievements AS sa
+                INNER JOIN students AS s
+                ON sa.student_id = s.user_id
+                INNER JOIN users AS u
+                ON s.user_id = u.id';
+
+        if($class) {
+            $whereCondition = true;
+            $sql .= ' WHERE class='.$class;
+        }
+
+        if($speciality) {
+            $whereCondition = true;
+            $begining = $whereCondition ? ' AND' : ' WHERE';
+            $sql .= $begining . ' speciality_id='.$speciality;
+        }
+
+        if($interval) {
+            $intervals = $this->getIntervals($interval);
+            $begining = $whereCondition ? ' AND' : ' WHERE';
+            $sql .= $begining . ' sa.date >= "' . $intervals['from'] . '" AND sa.date <= "' . $intervals['to'] . '"';
+        }
+
+        $sql .= ' GROUP BY student_id ORDER BY SUM(points) DESC';
+
+        $query = $this->dbConnection->query(
+            $sql
+        );
+
+        var_dump($sql);
+
+        $data = array();
+        while ($line = $query->fetch_assoc()) {
+            $student = array();
+            $student['id'] = $line['user_id'];
+            $student['fn'] = $line['fn'];
+            $student['name'] = $line['first_name'] . ' ' . $line['last_name'];
+            $student['points'] = $line['points'];
+            $data[] = $student;
         }
 
         return $data;
