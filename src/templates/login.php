3b7b845a3e40cc3f9714c3407c68cd1fa89bbea0
dakatapetrov <daka7a@gmail.com>
2015-02-06 09:17:04 +0200

    Fix login view messages

--- src/templates/login.php
+++ src/templates/login.php
@@ -1,7 +1,9 @@
-<h1>Вашата класация, господарю!</h1>
+<h1>Влизане:</h1>
 
 <form name="login" action="/sas/login" method="POST">
+Потребителско  име:<br />
 <input name="username" type="text"><br />
+Парола:<br />
 <input name="password" type="password"><br />
 <input type="submit" value="Влез">
 </form>
